7bf656277c8e96f74672948f1cc31e7560f27841
고범석 <kobums@naver.com>
2026-08-07 15:15:49 +0900

    chore: 쇼텐토텐·지킬앤하이드 WS 엔드포인트 등록

--- main.go
+++ main.go
@@ -19,6 +19,14 @@ func main() {
 	dvHub := server.NewDVHub()
 	go dvHub.Run()
 
+	// 쇼텐토텐 게임 허브
+	stHub := server.NewSTHub()
+	go stHub.Run()
+
+	// 지킬앤하이드 게임 허브
+	jhHub := server.NewJHHub()
+	go jhHub.Run()
+
 	// 구룡투 WebSocket 엔드포인트
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		server.ServeWs(hub, w, r)
@@ -34,10 +42,22 @@ func main() {
 		server.ServeDVWs(dvHub, w, r)
 	})
 
+	// 쇼텐토텐 WebSocket 엔드포인트
+	http.HandleFunc("/ws/schottentotten", func(w http.ResponseWriter, r *http.Request) {
+		server.ServeSTWs(stHub, w, r)
+	})
+
+	// 지킬앤하이드 WebSocket 엔드포인트
+	http.HandleFunc("/ws/jekyllhyde", func(w http.ResponseWriter, r *http.Request) {
+		server.ServeJHWs(jhHub, w, r)
+	})
+
 	log.Println("Server starting on :8003")
 	log.Println("  - Nine Dragons: /ws")
 	log.Println("  - Number Change: /ws/numberchange")
 	log.Println("  - DaVinci Code: /ws/davinci")
+	log.Println("  - Schotten Totten: /ws/schottentotten")
+	log.Println("  - Jekyll vs Hyde: /ws/jekyllhyde")
 	if err := http.ListenAndServe(":8003", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
